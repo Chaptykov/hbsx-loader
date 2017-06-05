efe02815a609fc02dc0177a1cd1fd93953623866
Timophy Chaptykov <tim.chaptykov@gmail.com>
2017-06-06 02:19:06 +0300

    Fix for old node

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const bluehtml = require('bluehtml');
 
 module.exports = function(source) {
-  return `
-const React = require('preact');
-const reactCreateElementWrapper = React.h;
-const _ = require('underscore');
-
-module.exports = function(ctx) {
-  return ${bluehtml.generateVirtualDOM(source, { notGenerateContext: true, adapterName: 'R' })}
-};`;
+  return '\
+const React = require(\'preact\');\
+const reactCreateElementWrapper = React.h;\
+const _ = require(\'underscore\');\
+\
+module.exports = function(ctx) {\
+  return ' + bluehtml.generateVirtualDOM(source, { notGenerateContext: true, adapterName: 'R' }) + ';\
+};';
 };
 
 // @TODO
